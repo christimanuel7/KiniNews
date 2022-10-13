7464f782c946d7a2b015e9385b24a2fe2dd28dab
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-10-13 20:15:41 +0700

    LogReg Intent Done (no DB) 2.0

--- app/src/main/java/com/example/kininews/MainActivity.java
+++ app/src/main/java/com/example/kininews/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
--- app/src/main/java/com/example/kininews/Register.java
+++ app/src/main/java/com/example/kininews/Register.java
@@ -1,14 +1,77 @@
 package com.example.kininews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView btnLogin;
+    Button btnRegister,btnClear;
+    EditText editFullName, editUserName, editEmail, editPassword, editRepeatPassword;
+    ImageButton btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        toolbar=findViewById(R.id.toolbar);
+        btnCancel=findViewById(R.id.btnCancel);
+        editUserName=findViewById(R.id.editUserName);
+        editFullName=findViewById(R.id.editFullName);
+        editEmail=findViewById(R.id.editEmail);
+        editPassword=findViewById(R.id.editPassword);
+        editRepeatPassword=findViewById(R.id.editRepeatPassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        btnClear=findViewById(R.id.btnClear);
+        btnLogin=findViewById(R.id.btnLogin);
+
+        setSupportActionBar(toolbar);
+
+//        LOGIN REGISTER DAN MASUK KE LOGIN MAIN ACTIVITY
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(Register.this, MainActivity.class);
+                startActivity(login);
+            }
+        });
+
+//        BERALIH KE REGISTER ACTIVITY
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gologin = new Intent(Register.this, MainActivity.class);
+                startActivity(gologin);
+            }
+        });
+
+//        KELUAR ACTIVITY APLIKASI
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAffinity();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editUserName.setText("");
+                editFullName.setText("");
+                editEmail.setText("");
+                editPassword.setText("");
+                editRepeatPassword.setText("");
+            }
+        });
     }
 }
