b33a4ac9600e6b8a0702be669bd0459b993bec86
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-10-26 21:27:11 +0700

    Minor - Clear Focus

--- app/src/main/java/com/example/kininews/Home.java
+++ app/src/main/java/com/example/kininews/Home.java
@@ -3,8 +3,13 @@ package com.example.kininews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
+    Toolbar toolbar;
+    ImageButton btnAccount, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
